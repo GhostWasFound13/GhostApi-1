20a904eb4e025785507ccd16a126be402ff3c1e2
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-09-25 11:55:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const fs = require('fs');
-const path = require('path'); // Import the 'path' module
-// Function to get a random GIF file from a folder
-function getRandomGIF(folder) {
-  const files = fs.readdirSync(folder).filter(file => file.endsWith('.gif'));
-  const randomIndex = Math.floor(Math.random() * files.length);
-  const randomGIF = files[randomIndex];
-  return path.join(__dirname, folder, randomGIF); // Use __dirname to create an absolute path
-}
-// Array of random facts
-const randomFacts = [
-  'Bananas are berries, but strawberries are not.',
-  'Honey never spoils; archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible.',
-  'Octopuses have three hearts.',
-  'The Eiffel Tower can be 15 cm taller during the summer due to the expansion of the iron it is made of.',
-  // Add more random facts as needed
-];
 
-// Function to get a random fact
-function getRandomFact() {
-  const randomIndex = Math.floor(Math.random() * randomFacts.length);
-  return randomFacts[randomIndex];
-}
+// Import your route loader
+const routes = require('./routes');
 
-// Define folders for each action
-const actionFolders = {
-  slap: './public/slap',
-  hug: './public/hug',
-  // Add more actions and their folders as needed
-};
+// Use the routes
+app.use('/', routes);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Anime Action and Random Facts API!');
-});
-
-// Endpoint for the /slap action
-app.get('/slap', (req, res) => {
-  const randomGIF = getRandomGIF(actionFolders.slap);
-  res.sendFile(randomGIF);
-});
-
-// Endpoint for the /hug action
-app.get('/hug', (req, res) => {
-  const randomGIF = getRandomGIF(actionFolders.hug);
-  res.sendFile(randomGIF);
-});
-
-// Endpoint for a random fact
-app.get('/randomfact', (req, res) => {
-  const randomFact = getRandomFact();
-  res.send(randomFact);
-});
-
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
